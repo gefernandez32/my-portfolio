5d4fb0049c3791855594106e7d1398f76410a63b
Gabriel Fernández <fernandez.emir@gmail.com>
2025-03-27 21:08:55 -0300

    ajustes

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,7 +1,7 @@
 export const personalInfo = {
   name: "Laura Molina",
   location: "Buenos Aires, Argentina",
-  email: "laura.monilna@gmail.com",
+  email: "smcc.4@hotmail.com ",
   github: "https://github.com/lauramolina",
   linkedin: "https://www.linkedin.com/in/laura-molina-5aa395249/",
 };
@@ -24,13 +24,23 @@ export const education = [
     institution: "Udemy",
     location: "Udemy",
     degree: "Python para Data Science & Machine Learning",
-    period: "Jun 2021 - Jun 2025",
+    period: "Nov 2024",
     achievements: [
 
       "Aprendí a utilizar Python para la ciencia de datos y el aprendizaje automático.",
       "Desarrollé habilidades en análisis de datos, visualización y modelado predictivo.",
       "Realicé proyectos prácticos para aplicar los conceptos aprendidos.",
-
+    ],
+  },
+  {
+    institution: "Udemy",
+    location: "Udemy",
+    degree: "Guía definitiva de DAX para Power Bi y Power Pivot",
+    period: "Ago 2023",
+    achievements: [
+      "Aprendí a utilizar DAX para el análisis de datos en Power BI y Power Pivot.",
+      "Desarrollé habilidades en la creación de medidas y cálculos avanzados.",
+      "Realicé proyectos prácticos para aplicar los conceptos aprendidos.",
 
     ],
   },
@@ -39,7 +49,7 @@ export const education = [
     institution: "Udemy",
     location: "Udemy",
     degree: "Curso Completo Power BI Desktop - Última versión de Power BI",
-    period: "Jun 2021 - Jun 2025",
+    period: "Jul 2023",
     achievements: [
       "Aprendí a utilizar Power BI para la visualización y análisis de datos.",
       "Desarrollé habilidades en la creación de informes interactivos y paneles de control.",
@@ -48,23 +58,12 @@ export const education = [
     ],
   },
 
-  {
-    institution: "Udemy",
-    location: "Udemy",
-    degree: "Guía definitiva de DAX para Power Bi y Power Pivot",
-    period: "Jun 2021 - Jun 2025",
-    achievements: [
-      "Aprendí a utilizar DAX para el análisis de datos en Power BI y Power Pivot.",
-      "Desarrollé habilidades en la creación de medidas y cálculos avanzados.",
-      "Realicé proyectos prácticos para aplicar los conceptos aprendidos.",
 
-    ],
-  },
   {
     institution: "Udemy",
     location: "Udemy",
     degree: "2023 Gestión Ágil de Proyectos con Scrum: Curso AGILE",
-    period: "Jun 2021 - Jun 2025",
+    period: "May 2023",
     achievements: [
       "Aprendí a gestionar proyectos utilizando la metodología ágil Scrum.",
       "Desarrollé habilidades en la planificación, ejecución y seguimiento de proyectos.",
@@ -76,7 +75,7 @@ export const education = [
     institution: "Udemy",
     location: "Udemy",
     degree: "Curso SQL Server Nivel Básico - Avanzado",
-    period: "Jun 2021 - Jun 2025",
+    period: "Mar 2023",
     achievements: [
       "Aprendí a utilizar SQL Server para la gestión y análisis de datos.",
       "Desarrollé habilidades en la creación de bases de datos, consultas y procedimientos almacenados.",
@@ -88,7 +87,7 @@ export const education = [
     institution: "Certiprof",
     location: "Certiprof",
     degree: "Certificado Profesional de la Fundación Scrum - SFPC™ (v2020)",
-    period: "Jun 2021 - Jun 2025",
+    period: "Feb 2023",
     achievements: [
 
       "Certificación profesional en la metodología Scrum.",
@@ -101,7 +100,7 @@ export const education = [
     institution: "Noble Work Foundation",
     location: "Noble Work Foundation",
     degree: "Análisis en Microsoft Excel: Power Pivot, Dax y Power Query",
-    period: "Jun 2021 - Jun 2025",
+    period: "Ene 2023",
     achievements: [
       "Aprendí a utilizar Microsoft Excel para el análisis de datos.",
       "Desarrollé habilidades en Power Pivot, DAX y Power Query.",
@@ -113,7 +112,7 @@ export const education = [
     institution: "Certiprof",
     location: "Certiprof",
     degree: "Certificación profesional de Business Intelligence Foundation - BIFPC™",
-    period: "Jun 2021 - Jun 2025",
+    period: "Ene 2023",
     achievements: [
       "Certificación profesional en Business Intelligence.",
       "Desarrollé habilidades en la gestión y análisis de datos.",
@@ -125,7 +124,7 @@ export const education = [
     institution: "Certiprof",
     location: "Certiprof",
     degree: "Curso de Power BI - Análisis de Datos y Business Intelligence",
-    period: "Jun 2021 - Jun 2025",
+    period: "Oct 2022",
     achievements: [
       "Aprendí a utilizar Power BI para el análisis de datos y Business Intelligence.",
       "Desarrollé habilidades en la creación de informes y paneles de control.",
