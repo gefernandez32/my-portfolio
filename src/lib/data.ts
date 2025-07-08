20f4d22c1b26c9b415fafd5731f91e9816b0e5b0
Gabriel Fernández <fernandez.emir@gmail.com>
2025-07-08 10:21:04 -0300

    Refactor code structure for improved readability and maintainability

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -16,7 +16,6 @@ export const workExperience = [
       "Soy Analista de Datos con experiencia en Business Intelligence. Mi trabajo consiste en crear informes, paneles de control y métricas utilizando Microsoft Power BI. Mi objetivo principal es proporcionar información precisa para mejorar la toma de decisiones. Utilizo herramientas como Microsoft Power BI, Excel Power Pivot y SQL Server.",
     ],
   },
-
 ];
 
 export const education = [
@@ -26,7 +25,6 @@ export const education = [
     degree: "Python para Data Science & Machine Learning",
     period: "Nov 2024",
     achievements: [
-
       "Aprendí a utilizar Python para la ciencia de datos y el aprendizaje automático.",
       "Desarrollé habilidades en análisis de datos, visualización y modelado predictivo.",
       "Realicé proyectos prácticos para aplicar los conceptos aprendidos.",
@@ -41,7 +39,6 @@ export const education = [
       "Aprendí a utilizar DAX para el análisis de datos en Power BI y Power Pivot.",
       "Desarrollé habilidades en la creación de medidas y cálculos avanzados.",
       "Realicé proyectos prácticos para aplicar los conceptos aprendidos.",
-
     ],
   },
 
@@ -54,10 +51,8 @@ export const education = [
       "Aprendí a utilizar Power BI para la visualización y análisis de datos.",
       "Desarrollé habilidades en la creación de informes interactivos y paneles de control.",
       "Realicé proyectos prácticos para aplicar los conceptos aprendidos.",
-
     ],
   },
-
 
   {
     institution: "Udemy",
@@ -68,7 +63,6 @@ export const education = [
       "Aprendí a gestionar proyectos utilizando la metodología ágil Scrum.",
       "Desarrollé habilidades en la planificación, ejecución y seguimiento de proyectos.",
       "Realicé proyectos prácticos para aplicar los conceptos aprendidos.",
-
     ],
   },
   {
@@ -80,7 +74,6 @@ export const education = [
       "Aprendí a utilizar SQL Server para la gestión y análisis de datos.",
       "Desarrollé habilidades en la creación de bases de datos, consultas y procedimientos almacenados.",
       "Realicé proyectos prácticos para aplicar los conceptos aprendidos.",
-
     ],
   },
   {
@@ -89,11 +82,9 @@ export const education = [
     degree: "Certificado Profesional de la Fundación Scrum - SFPC™ (v2020)",
     period: "Feb 2023",
     achievements: [
-
       "Certificación profesional en la metodología Scrum.",
       "Desarrollé habilidades en la gestión de proyectos ágiles.",
       "Participé en proyectos prácticos para aplicar los conceptos aprendidos.",
-
     ],
   },
   {
@@ -105,19 +96,18 @@ export const education = [
       "Aprendí a utilizar Microsoft Excel para el análisis de datos.",
       "Desarrollé habilidades en Power Pivot, DAX y Power Query.",
       "Realicé proyectos prácticos para aplicar los conceptos aprendidos.",
-
     ],
   },
   {
     institution: "Certiprof",
     location: "Certiprof",
-    degree: "Certificación profesional de Business Intelligence Foundation - BIFPC™",
+    degree:
+      "Certificación profesional de Business Intelligence Foundation - BIFPC™",
     period: "Ene 2023",
     achievements: [
       "Certificación profesional en Business Intelligence.",
       "Desarrollé habilidades en la gestión y análisis de datos.",
       "Participé en proyectos prácticos para aplicar los conceptos aprendidos.",
-
     ],
   },
   {
@@ -129,18 +119,11 @@ export const education = [
       "Aprendí a utilizar Power BI para el análisis de datos y Business Intelligence.",
       "Desarrollé habilidades en la creación de informes y paneles de control.",
       "Realicé proyectos prácticos para aplicar los conceptos aprendidos.",
-
     ],
   },
 ];
 export const skills = {
-  programmingLanguages: [
-    "Transact SQL",
-    "DAX",
-    "Python",
-    "R",
-    "M"
-  ],
+  programmingLanguages: ["Transact SQL", "DAX", "Python", "R", "M"],
   frontendDevelopment: [
     "React",
     "Nextjs",
@@ -165,8 +148,7 @@ export const projects = [
   {
     title: "🚗 Análisis de Flujo Vehicular y Seguridad Vial en Autopistas AUSA",
     description: [
-      "Este proyecto analiza datos de siniestralidad y flujo vehicular en las autopistas de la Ciudad Autónoma de Buenos Aires (AUSA), con el objetivo de entender si existe relación entre el tráfico vehicular, las condiciones climáticas y los accidentes de tránsito."
-
+      "Este proyecto analiza datos de siniestralidad y flujo vehicular en las autopistas de la Ciudad Autónoma de Buenos Aires (AUSA), con el objetivo de entender si existe relación entre el tráfico vehicular, las condiciones climáticas y los accidentes de tránsito.",
     ],
     image: "ausa.png",
     link: "https://app.powerbi.com/view?r=eyJrIjoiY2I2NjdmN2MtZTZiZi00NzllLWE0YjUtNjYxNjFhOGQwNWJhIiwidCI6ImQxNDIxOTAxLWMxNzMtNGFiNS1hNGQ3LTlkYjE2NDNlNjFkMyIsImMiOjR9&pageName=c09ac4f179aac250c186",
@@ -177,7 +159,6 @@ export const projects = [
     description: [
       "Este proyecto consiste en el análisis de datos de un hipermercado con el objetivo de identificar patrones de ventas, productos rentables, comportamiento de clientes y rendimiento por segmento.",
       "Se utilizó Python para el tratamiento y análisis exploratorio de datos, y Power BI para el modelado y la visualización interactiva.",
-
     ],
     image: "superstore.png",
     link: "https://app.powerbi.com/view?r=eyJrIjoiNmNlN2UzNGMtNzU3Yi00M2NlLTgxMWQtZDFjNWRlYWI5M2NhIiwidCI6ImQxNDIxOTAxLWMxNzMtNGFiNS1hNGQ3LTlkYjE2NDNlNjFkMyIsImMiOjR9",
@@ -191,6 +172,24 @@ export const projects = [
     ],
     image: "tickets.png",
     link: "https://app.powerbi.com/view?r=eyJrIjoiNDFhODkxZGMtZjNlZC00YTRmLWFhNDctNTBmZjkzMDE4N2Y3IiwidCI6ImQxNDIxOTAxLWMxNzMtNGFiNS1hNGQ3LTlkYjE2NDNlNjFkMyIsImMiOjR9",
+    github: "#",
+  },
+  {
+    title: "📊 Monitoreo de Asistencia Laboral (Power BI)",
+    description: [
+      "Panel interactivo para visualizar el cumplimiento diario de jornada laboral, ausencias, vacaciones y desempeño por persona.",
+    ],
+    image: "monitoreo.png",
+    link: "https://app.powerbi.com/view?r=eyJrIjoiY2Y1NmFjMjktZjJmMi00NTI3LWJkMGUtY2YxNmJhMGYzYWUyIiwidCI6ImQxNDIxOTAxLWMxNzMtNGFiNS1hNGQ3LTlkYjE2NDNlNjFkMyIsImMiOjR9",
+    github: "#",
+  },
+  {
+    title: "📊 Reporte de Producción Minera Mundial (2019–2023)",
+    description: [
+      "Este proyecto es un análisis exploratorio y visual de la producción de minerales a nivel mundial, utilizando datos extraídos del sitio oficial de World Mining Data. El objetivo principal es identificar tendencias globales, principales países productores, distribución por tipo de mineral y evolución anual.",
+    ],
+    image: "mineria.png",
+    link: "https://app.powerbi.com/view?r=eyJrIjoiYWZjY2QyNmUtYTU5MS00Y2Q3LThhNTEtZmZhMDNlM2E3NTBiIiwidCI6ImQxNDIxOTAxLWMxNzMtNGFiNS1hNGQ3LTlkYjE2NDNlNjFkMyIsImMiOjR9",
     github: "#",
   },
 ];
